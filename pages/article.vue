fda614068709cdaf9b76aee0a6e5ce5262705577
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2020-12-30 15:46:21 +0200

    Create article.vue

---
+++ pages/article.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="p-6">
+
+  <div class="grid grid-cols-6 grid-rows-2 gap-2 p-6">
+    <div class="bg-gray-100"></div>
+    <div class="col-span-4 row-span-2 px-4 py-2 bg-gray-100 border-2 border-gray-200 rounded-md">
+      <h1 class="text-2xl font-semibold text-gray-800">Gönderi</h1>
+      <p class="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem maxime suscipit laboriosam, sit culpa.</p>
+      <ul>
+      <li class="float-left"><span class="badge">28/12/2020</span></li><li><span class="badge">#etiket</span></li>
+      </ul>
+    </div>
+    <div class="bg-gray-100"></div>
+    <div class="col-span-4 col-start-2 px-4 py-2 text-lg text-justify text-gray-800 bg-gray-100">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, aliquid aliquam? Porro eum natus praesentium nostrum placeat alias aliquam est, atque corporis aperiam sapiente enim eos unde illum quia vero! Architecto, dolorum. Sit doloremque in provident itaque modi vitae mollitia sequi officiis delectus fugit. Non vero rem unde dolorem nobis ducimus voluptatum, optio vel natus omnis magni quisquam minus! Dolor sed architecto nobis dicta commodi, voluptatem veniam quia iste consequatur expedita natus praesentium quod enim, doloribus, ipsam voluptatum ipsa ad ut. Illum id nesciunt libero at rerum repellat quibusdam magnam, rem reiciendis, molestiae voluptas repellendus qui aliquam est, expedita pariatur placeat inventore fugit! Maxime quisquam corporis deserunt vero doloribus fuga explicabo nam sint et in nobis corrupti iste enim cumque laborum aperiam ad officia voluptate quibusdam repellendus odio provident libero, ipsam nemo? Inventore accusantium dolorem in, minus, odit iste id recusandae eos voluptas optio quia culpa aspernatur blanditiis nihil fuga. Aliquid enim quo ad ex ipsum nisi aperiam, repellat doloribus vero commodi consequatur dolor delectus adipisci optio odit itaque reprehenderit corrupti ea magni illo laudantium porro autem accusamus eum. Ex omnis cum rem perferendis tempora nesciunt assumenda nam dolorum commodi earum! Itaque repudiandae ratione architecto quos quia, temporibus consectetur.</div>
+  </div>
+
+</div>
+</template>
+<style lang="postcss" scoped>
+.badge {
+  @apply text-lg text-gray-700 font-medium bg-gray-300 px-2 rounded-md inline-block mt-1 mr-2 mb-1;
+}
+</style>

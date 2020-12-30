352cbfaa16d03695851df0811a8379f86552af09
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2020-12-30 15:46:06 +0200

    Update blog.vue

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,68 +1,68 @@
 <template>
 <div class="p-6">
 
-  <h1 class="text-2xl font-semibold ml-6 -mb-5">SON GÖNDERİLER</h1>
+  <h1 class="ml-6 -mb-5 text-2xl font-semibold">SON GÖNDERİLER</h1>
   <div class="grid grid-cols-4 grid-rows-2 gap-2 p-6">
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-800 rounded-md border-2 border-gray-900 cursor-pointer col-span-2 row-span-2">
+    <NuxtLink to="/article" class="col-span-2 row-span-2 px-4 py-2 bg-gray-800 border-2 border-gray-900 rounded-md cursor-pointer">
       <h1 class="text-2xl font-semibold text-gray-300 truncate">Gönderi</h1>
       <p class="text-gray-200 truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem maxime suscipit laboriosam, sit culpa.</p>
       <ul>
       <li class="float-left"><span class="badge">28/12/2020</span></li><li><span class="badge">#etiket</span></li>
       </ul>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-green-700 rounded-md border-2 border-green-800 cursor-pointer hover:bg-green-800 col-span-2">
+    <NuxtLink to="/article" class="col-span-2 px-4 py-2 bg-green-700 border-2 border-green-800 rounded-md cursor-pointer hover:bg-green-800">
       <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-green-700 rounded-md border-2 border-green-800 cursor-pointer hover:bg-green-800">
+    <NuxtLink to="/article" class="px-4 py-2 bg-green-700 border-2 border-green-800 rounded-md cursor-pointer hover:bg-green-800">
       <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-green-700 rounded-md border-2 border-green-800 cursor-pointer hover:bg-green-800">
+    <NuxtLink to="/article" class="px-4 py-2 bg-green-700 border-2 border-green-800 rounded-md cursor-pointer hover:bg-green-800">
       <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
     </NuxtLink>
   </div>
-  <h1 class="text-2xl font-semibold ml-6 -mb-5">DISCORD</h1>
+  <h1 class="ml-6 -mb-5 text-2xl font-semibold">DISCORD</h1>
   <div class="grid grid-cols-2 gap-2 p-6">
     <div class="grid grid-cols-2 grid-rows-2 gap-2">
-      <NuxtLink to="/article" class="px-4 py-2 bg-indigo-500 rounded-md border-2 border-indigo-600 cursor-pointer hover:bg-indigo-600 col-span-2">
+      <NuxtLink to="/article" class="col-span-2 px-4 py-2 bg-indigo-500 border-2 border-indigo-600 rounded-md cursor-pointer hover:bg-indigo-600">
         <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
       </NuxtLink>
-      <NuxtLink to="/article" class="px-4 py-2 bg-indigo-500 rounded-md border-2 border-indigo-600 cursor-pointer hover:bg-indigo-600">
+      <NuxtLink to="/article" class="px-4 py-2 bg-indigo-500 border-2 border-indigo-600 rounded-md cursor-pointer hover:bg-indigo-600">
         <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
       </NuxtLink>
-      <NuxtLink to="/article" class="px-4 py-2 bg-indigo-500 rounded-md border-2 border-indigo-600 cursor-pointer hover:bg-indigo-600">
+      <NuxtLink to="/article" class="px-4 py-2 bg-indigo-500 border-2 border-indigo-600 rounded-md cursor-pointer hover:bg-indigo-600">
         <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
       </NuxtLink>
     </div>
     <div class="grid grid-cols-2 grid-rows-2 gap-2">
-      <NuxtLink to="/article" class="px-4 py-2 bg-purple-600 rounded-md border-2 border-purple-700 cursor-pointer hover:bg-purple-700">
+      <NuxtLink to="/article" class="px-4 py-2 bg-purple-600 border-2 border-purple-700 rounded-md cursor-pointer hover:bg-purple-700">
         <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
       </NuxtLink>
-      <NuxtLink to="/article" class="px-4 py-2 bg-purple-600 rounded-md border-2 border-purple-700 cursor-pointer hover:bg-purple-700">
+      <NuxtLink to="/article" class="px-4 py-2 bg-purple-600 border-2 border-purple-700 rounded-md cursor-pointer hover:bg-purple-700">
         <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
       </NuxtLink>
-      <NuxtLink to="/article" class="px-4 py-2 bg-purple-600 rounded-md border-2 border-purple-700 cursor-pointer hover:bg-purple-700 col-span-2">
+      <NuxtLink to="/article" class="col-span-2 px-4 py-2 bg-purple-600 border-2 border-purple-700 rounded-md cursor-pointer hover:bg-purple-700">
         <h1 class="text-2xl font-semibold text-gray-200 truncate">Gönderi</h1>
       </NuxtLink>
     </div>
   </div>
-  <h1 class="text-2xl font-semibold ml-6 -mb-5">DİĞER</h1>
+  <h1 class="ml-6 -mb-5 text-2xl font-semibold">DİĞER</h1>
   <div class="grid grid-cols-3 grid-rows-2 gap-2 p-6">
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 rounded-md border-2 border-gray-400 cursor-pointer hover:bg-gray-400">
+    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-400">
       <h1 class="text-2xl font-semibold text-gray-800 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 rounded-md border-2 border-gray-400 cursor-pointer hover:bg-gray-400">
+    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-400">
       <h1 class="text-2xl font-semibold text-gray-800 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 rounded-md border-2 border-gray-400 cursor-pointer hover:bg-gray-400">
+    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-400">
       <h1 class="text-2xl font-semibold text-gray-800 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 rounded-md border-2 border-gray-400 cursor-pointer hover:bg-gray-400">
+    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-400">
       <h1 class="text-2xl font-semibold text-gray-800 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 rounded-md border-2 border-gray-400 cursor-pointer hover:bg-gray-400">
+    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-400">
       <h1 class="text-2xl font-semibold text-gray-800 truncate">Gönderi</h1>
     </NuxtLink>
-    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 rounded-md border-2 border-gray-400 cursor-pointer hover:bg-gray-400">
+    <NuxtLink to="/article" class="px-4 py-2 bg-gray-300 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-400">
       <h1 class="text-2xl font-semibold text-gray-800 truncate">Gönderi</h1>
     </NuxtLink>
   </div>

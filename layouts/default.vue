415823ffddcd036b489946fd71cb54c83bfe28c7
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2020-12-30 16:44:43 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  font-family: 'DM Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;

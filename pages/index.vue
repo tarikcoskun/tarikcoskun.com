87af9ffaf0558168575a067dd54821d09870e73c
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2020-12-30 15:45:42 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,37 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+<div class="p-8">
+
+  <div class="grid grid-cols-4 gap-4 p-6 divide-x-2 divide-gray-100">
+    <div class="px-6 py-2">
+      <h1 class="text-4xl font-semibold">1st Title</h1>
+    </div>
+    <div class="col-span-2 px-6 py-2">
+      <h1 class="text-4xl font-semibold">2nd Title</h1>
+    </div>
+    <div class="col-span-2 col-start-4 px-6 py-2">
+      <h1 class="text-4xl font-semibold">Skills</h1>
+      <p class="text-xl font-semibold text-gray-700">HTML</p>
+      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
+        <div class="flex flex-col justify-center w-full text-white whitespace-nowrap bg-gradient-to-r from-red-500 to-yellow-500"></div>
+      </div>
+      <p class="text-xl font-semibold text-gray-700">CSS</p>
+      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
+        <div class="flex flex-col justify-center w-4/6 text-white whitespace-nowrap bg-gradient-to-r from-indigo-500 to-blue-400"></div>
+      </div>
+      <p class="text-xl font-semibold text-gray-700">JavaScript</p>
+      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
+        <div class="flex flex-col justify-center w-10/12 text-white whitespace-nowrap bg-gradient-to-r from-yellow-500 to-yellow-300"></div>
+      </div>
+      <p class="text-xl font-semibold text-gray-700">NodeJS</p>
+      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
+        <div class="flex flex-col justify-center w-9/12 text-white whitespace-nowrap bg-gradient-to-r from-green-600 to-green-400"></div>
+      </div>
+      <p class="text-xl font-semibold text-gray-700">NuxtJS</p>
+      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
+        <div class="flex flex-col justify-center w-1/12 text-white whitespace-nowrap bg-gradient-to-r from-green-800 to-green-700"></div>
       </div>
     </div>
   </div>
+
+</div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>

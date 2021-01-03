4f53ea1ff01f0b14aa45ce9b291d8500ad503387
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-03 20:50:15 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,71 @@
 <template>
-<div class="p-8">
+<div class="px-12 py-6">
 
-  <div class="grid grid-cols-4 gap-4 p-6 divide-x-2 divide-gray-100">
-    <div class="px-6 py-2">
-      <h1 class="text-4xl font-semibold">1st Title</h1>
+  <div class="grid grid-cols-7 gap-4">
+    <div class="col-span-4 p-3 bg-gray-100 rounded-lg">
+      <h1 class="text-2xl font-semibold">About Me</h1>
+      <p class="text-lg text-justify text-gray-700">Howdy! I'm Tarık, a young developer who is trying to create websites using new technologies he learns. Wants to learn game developing languages to help friends and create his own one, one day. Also wants to learn foreign languages to translate movie subtitles, games, websites etc.</p>
     </div>
-    <div class="col-span-2 px-6 py-2">
-      <h1 class="text-4xl font-semibold">2nd Title</h1>
+    <div class="col-span-3 p-3 bg-gray-100 rounded-lg">
+      <h1 class="text-2xl font-semibold">Skills</h1>
+      <div class="grid grid-cols-2 gap-2">
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <span class="inline-block text-xl font-semibold text-gray-700">JavaScript</span>
+            <div class="text-right">
+              <span class="inline-block text-lg font-semibold text-gray-600">75%</span>
+            </div>
+          </div>
+          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
+            <div class="flex flex-col justify-center w-9/12 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          </div>
+        </div>
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <span class="inline-block text-xl font-semibold text-gray-700">NodeJS</span>
+            <div class="text-right">
+              <span class="inline-block text-lg font-semibold text-gray-600">50%</span>
+            </div>
+          </div>
+          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
+            <div class="flex flex-col justify-center w-3/6 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          </div>
+        </div>
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <span class="inline-block text-xl font-semibold text-gray-700">NuxtJS</span>
+            <div class="text-right">
+              <span class="inline-block text-lg font-semibold text-gray-600">15%</span>
+            </div>
+          </div>
+          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
+            <div class="flex flex-col justify-center w-1/6 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          </div>
+        </div>
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <span class="inline-block text-xl font-semibold text-gray-700">TailwindCSS</span>
+            <div class="text-right">
+              <span class="inline-block text-lg font-semibold text-gray-600">40%</span>
+            </div>
+          </div>
+          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
+            <div class="flex flex-col justify-center w-2/5 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col-span-2 col-start-4 px-6 py-2">
-      <h1 class="text-4xl font-semibold">Skills</h1>
-      <p class="text-xl font-semibold text-gray-700">HTML</p>
-      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
-        <div class="flex flex-col justify-center w-full text-white whitespace-nowrap bg-gradient-to-r from-red-500 to-yellow-500"></div>
-      </div>
-      <p class="text-xl font-semibold text-gray-700">CSS</p>
-      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
-        <div class="flex flex-col justify-center w-4/6 text-white whitespace-nowrap bg-gradient-to-r from-indigo-500 to-blue-400"></div>
-      </div>
-      <p class="text-xl font-semibold text-gray-700">JavaScript</p>
-      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
-        <div class="flex flex-col justify-center w-10/12 text-white whitespace-nowrap bg-gradient-to-r from-yellow-500 to-yellow-300"></div>
-      </div>
-      <p class="text-xl font-semibold text-gray-700">NodeJS</p>
-      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
-        <div class="flex flex-col justify-center w-9/12 text-white whitespace-nowrap bg-gradient-to-r from-green-600 to-green-400"></div>
-      </div>
-      <p class="text-xl font-semibold text-gray-700">NuxtJS</p>
-      <div class="flex h-4 mb-4 overflow-hidden text-xs bg-gray-200 rounded-full">
-        <div class="flex flex-col justify-center w-1/12 text-white whitespace-nowrap bg-gradient-to-r from-green-800 to-green-700"></div>
+    <div class="col-span-2 p-3 bg-gray-100 rounded-lg">
+      <h1 class="text-2xl font-semibold">Experiences</h1>
+      <div class="grid gap-2">
+        <div>
+          <h1 class="text-lg font-semibold text-gray-800">Project</h1>
+          <p class="-mt-1 text-lg text-gray-700">Position</p>
+        </div>
+        <div>
+          <h1 class="text-lg font-semibold text-gray-800">Project</h1>
+          <p class="-mt-1 text-lg text-gray-700">Position</p>
+        </div>
       </div>
     </div>
   </div>

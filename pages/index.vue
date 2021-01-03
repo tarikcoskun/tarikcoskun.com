80de5879af226e1525c23bd5f1e4615f967fd6d5
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-03 23:22:32 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-<div class="px-12 py-6">
+<div class="px-16 py-8">
 
-  <div class="grid grid-cols-7 gap-4">
-    <div class="col-span-4 p-3 bg-gray-100 rounded-lg">
+  <div class="grid-cols-7 gap-4 md:grid">
+    <div class="col-span-4 p-3 mb-4 bg-gray-100 rounded-lg">
       <h1 class="text-2xl font-semibold">About Me</h1>
-      <p class="text-lg text-justify text-gray-700">Howdy! I'm Tarık, a young developer who is trying to create websites using new technologies he learns. Wants to learn game developing languages to help friends and create his own one, one day. Also wants to learn foreign languages to translate movie subtitles, games, websites etc.</p>
+      <p class="text-lg text-justify text-gray-700">Howdy! I'm Tarık, a young developer from Turkey. I spend most of my time playing games, coding and chatting with my friends. I have been coding since 2018, when I started using Discord. I've always wondered how Discord bots and websites work, so I started researching about them. About 8 months later, I had enough knowledge to create Discord bots from scratch. After learning about Discord bots, I started researching about websites and since then, I'm trying to create websites using technologies I'm learning.</p>
     </div>
     <div class="col-span-3 p-3 bg-gray-100 rounded-lg">
       <h1 class="text-2xl font-semibold">Skills</h1>
-      <div class="grid grid-cols-2 gap-2">
+      <div class="grid gap-2 md:grid-cols-2">
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700">JavaScript</span>
+            <span class="inline-block text-xl font-semibold text-gray-700 truncate">JavaScript</span>
             <div class="text-right">
               <span class="inline-block text-lg font-semibold text-gray-600">75%</span>
             </div>
@@ -22,7 +22,18 @@
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700">NodeJS</span>
+            <span class="inline-block text-xl font-semibold text-gray-700 truncate">CSS</span>
+            <div class="text-right">
+              <span class="inline-block text-lg font-semibold text-gray-600">60%</span>
+            </div>
+          </div>
+          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
+            <div class="flex flex-col justify-center w-3/5 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          </div>
+        </div>
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <span class="inline-block text-xl font-semibold text-gray-700 truncate">NodeJS</span>
             <div class="text-right">
               <span class="inline-block text-lg font-semibold text-gray-600">50%</span>
             </div>
@@ -33,7 +44,7 @@
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700">NuxtJS</span>
+            <span class="inline-block text-xl font-semibold text-gray-700 truncate">NuxtJS</span>
             <div class="text-right">
               <span class="inline-block text-lg font-semibold text-gray-600">15%</span>
             </div>
@@ -44,7 +55,7 @@
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700">TailwindCSS</span>
+            <span class="inline-block text-xl font-semibold text-gray-700 truncate">TailwindCSS</span>
             <div class="text-right">
               <span class="inline-block text-lg font-semibold text-gray-600">40%</span>
             </div>
@@ -53,18 +64,16 @@
             <div class="flex flex-col justify-center w-2/5 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="col-span-2 p-3 bg-gray-100 rounded-lg">
-      <h1 class="text-2xl font-semibold">Experiences</h1>
-      <div class="grid gap-2">
-        <div>
-          <h1 class="text-lg font-semibold text-gray-800">Project</h1>
-          <p class="-mt-1 text-lg text-gray-700">Position</p>
-        </div>
-        <div>
-          <h1 class="text-lg font-semibold text-gray-800">Project</h1>
-          <p class="-mt-1 text-lg text-gray-700">Position</p>
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <span class="inline-block text-xl font-semibold text-gray-700 truncate">GameMaker Studio Language</span>
+            <div class="text-right">
+              <span class="inline-block text-lg font-semibold text-gray-600">10%</span>
+            </div>
+          </div>
+          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
+            <div class="flex flex-col justify-center w-1/12 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          </div>
         </div>
       </div>
     </div>

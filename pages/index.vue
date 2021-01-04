7940e1bdbbaa00f333487d6e7e391e851266a240
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-04 11:06:16 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,32 @@
 <div class="p-4 md:px-16 md:py-8">
 
   <div class="gap-4 md:grid md:grid-cols-7">
-    <section class="col-span-4">
-    <div class="col-span-4 p-3 mb-4 bg-gray-100 rounded-lg md:mb-0">
+    <div class="col-span-7 p-3 bg-gray-100 rounded-lg md:p-6 md:mb-0">
       <h1 class="text-2xl font-semibold">About Me</h1>
-      <p class="text-lg text-justify text-gray-700">Howdy! I'm Tarık, a young developer from Turkey. I spend most of my time playing games, coding and chatting with my friends. I have been coding since 2018, when I started using Discord. I've always wondered how Discord bots and websites work, so I started researching about them. About 8 months later, I had enough knowledge to create Discord bots from scratch. After learning about Discord bots, I started researching about websites and since then, I'm trying to create websites using technologies I'm learning.</p>
+      <p class="text-lg text-justify text-gray-700">Howdy! I'm Tarık, a young developer from Turkey. I spend most of my time playing games, coding and chatting. I have been coding since 2018, when I started using Discord. I've always wondered how Discord bots and websites work, so I started researching about them. About 8 months later, I had enough knowledge to create Discord bots from scratch. After learning about Discord bots, I started researching about websites and since then, I'm trying to create websites using new technologies I learn.</p>
     </div>
+    <section class="col-span-4 mt-4 md:mt-0">
+      <div class="col-span-7 p-3 mb-4 bg-gray-100 rounded-lg md:mb-0">
+        <h1 class="text-2xl font-semibold">Project</h1>
+        <div class="grid gap-2 md:grid-cols-2">
+          <div>
+            <a href="https://zyapimstudios.com/generations" target="_blank" class="text-xl font-semibold text-gray-800 hover:underline">Generations</a>
+            <p class="-mt-1 text-lg text-gray-700">Website Developer</p>
+          </div>
+          <div>
+            <a href="#" target="_blank" class="text-xl font-semibold text-gray-800 hover:underline">Project Name</a>
+            <p class="-mt-1 text-lg text-gray-700">Position</p>
+          </div>
+          <div>
+            <a href="#" target="_blank" class="text-xl font-semibold text-gray-800 hover:underline">Project Name</a>
+            <p class="-mt-1 text-lg text-gray-700">Position</p>
+          </div>
+          <div>
+            <a href="#" target="_blank" class="text-xl font-semibold text-gray-800 hover:underline">Project Name</a>
+            <p class="-mt-1 text-lg text-gray-700">Position</p>
+          </div>
+        </div>
+      </div>
     </section>
     <section class="col-span-3">
     <div class="p-3 bg-gray-100 rounded-lg">
@@ -14,57 +35,56 @@
       <div class="grid gap-2 md:grid-cols-2">
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700 truncate">JavaScript</span>
-            <div class="text-right">
-              <span class="inline-block text-lg font-semibold text-gray-600">75%</span>
-            </div>
+            <h1 class="text-xl font-semibold text-gray-700 truncate">HTML</h1>
+            <span class="text-lg font-semibold text-right text-gray-600">95%</span>
           </div>
-          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
-            <div class="flex flex-col justify-center w-9/12 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          <div class="flex h-2 overflow-hidden bg-gray-400 rounded">
+            <div class="w-11/12 bg-gray-700"></div>
           </div>
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700 truncate">CSS</span>
-            <div class="text-right">
-              <span class="inline-block text-lg font-semibold text-gray-600">60%</span>
-            </div>
+            <h1 class="text-xl font-semibold text-gray-700 truncate">JavaScript</h1>
+            <span class="text-lg font-semibold text-right text-gray-600">75%</span>
           </div>
-          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
-            <div class="flex flex-col justify-center w-3/5 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          <div class="flex h-2 overflow-hidden bg-gray-400 rounded">
+            <div class="w-9/12 bg-gray-700"></div>
           </div>
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700 truncate">NodeJS</span>
-            <div class="text-right">
-              <span class="inline-block text-lg font-semibold text-gray-600">50%</span>
-            </div>
+            <h1 class="text-xl font-semibold text-gray-700 truncate">CSS</h1>
+            <span class="text-lg font-semibold text-right text-gray-600">70%</span>
           </div>
-          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
-            <div class="flex flex-col justify-center w-3/6 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          <div class="flex h-2 overflow-hidden bg-gray-400 rounded">
+            <div class="w-2/3 bg-gray-700"></div>
           </div>
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700 truncate">NuxtJS</span>
-            <div class="text-right">
-              <span class="inline-block text-lg font-semibold text-gray-600">15%</span>
-            </div>
+            <h1 class="text-xl font-semibold text-gray-700 truncate">NodeJS</h1>
+            <span class="text-lg font-semibold text-right text-gray-600">60%</span>
           </div>
-          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
-            <div class="flex flex-col justify-center w-1/6 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          <div class="flex h-2 overflow-hidden bg-gray-400 rounded">
+            <div class="w-3/5 bg-gray-700"></div>
           </div>
         </div>
         <div class="p-2 bg-gray-200 rounded-md">
           <div class="flex items-center justify-between mb-1">
-            <span class="inline-block text-xl font-semibold text-gray-700 truncate">TailwindCSS</span>
-            <div class="text-right">
-              <span class="inline-block text-lg font-semibold text-gray-600">40%</span>
-            </div>
+            <h1 class="text-xl font-semibold text-gray-700 truncate">NuxtJS</h1>
+            <span class="text-lg font-semibold text-right text-gray-600">15%</span>
           </div>
-          <div class="flex h-2 overflow-hidden text-xs bg-gray-400 rounded">
-            <div class="flex flex-col justify-center w-2/5 text-center text-white bg-gray-700 shadow-none whitespace-nowrap"></div>
+          <div class="flex h-2 overflow-hidden bg-gray-400 rounded">
+            <div class="w-1/6 bg-gray-700"></div>>
+          </div>
+        </div>
+        <div class="p-2 bg-gray-200 rounded-md">
+          <div class="flex items-center justify-between mb-1">
+            <h1 class="text-xl font-semibold text-gray-700 truncate">TailwindCSS</h1>
+            <span class="text-lg font-semibold text-right text-gray-600">40%</span>
+          </div>
+          <div class="flex h-2 overflow-hidden bg-gray-400 rounded">
+            <div class="w-2/5 bg-gray-700"></div>
           </div>
         </div>
       </div>

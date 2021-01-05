2fd2f8c45e004487ed754f869fc188385f5b6e76
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-05 15:10:30 +0200

    Update _slug.vue

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -2,8 +2,8 @@
 <div class="p-4 md:px-48 md:py-8">
 
   <div class="grid gap-2">
-    <div class="p-3 bg-gray-200 rounded-lg">
-      <h1 class="text-2xl font-semibold text-gray-800">{{ article.title }}</h1>
+    <div class="p-3 bg-gray-100 rounded-lg">
+      <h1 class="text-2xl text-gray-700">{{ article.title }}</h1>
       <p class="text-justify text-gray-700">{{ article.description }}</p>
     </div>
     <nuxt-content :document="article" class="text-lg text-justify text-gray-800" />
@@ -12,57 +12,49 @@
 </div>
 </template>
 
-<style lang="postcss">
+<style lang="scss">
 .nuxt-content {
   @apply leading-relaxed;
-}
-.nuxt-content h1 {
-  @apply font-semibold mt-2 text-3xl;
-}
-.nuxt-content h2 {
-  @apply font-semibold mt-2 text-2xl;
-}
-.nuxt-content h3 {
-  @apply font-semibold mt-2 text-xl;
-}
-.nuxt-content a {
-  @apply text-blue-600 hover:text-blue-800 hover:underline;
-}
-.nuxt-content code {
-  @apply bg-gray-800 text-gray-400 px-1 rounded py-px;
-}
-.nuxt-content blockquote {
-  @apply py-1 px-4  bg-gray-100 mb-4 border-l-4 border-gray-400;
-}
-.nuxt-content blockquote > blockquote {
-   @apply bg-gray-200;
- }
-.nuxt-content pre {
-  @apply bg-gray-800 rounded-md;
-}
-.nuxt-content ol {
-  @apply list-decimal ml-8;
-}
-.nuxt-content ul {
-  @apply list-disc ml-8;
-}
-.nuxt-content hr {
-  @apply my-2;
-}
-.nuxt-content td, th {
-  @apply px-2 py-1 border border-gray-400;
-}
-.nuxt-content tr:nth-child(odd) {
-  @apply bg-gray-100;
-}
-.nuxt-content table {
-  @apply mb-6;
+  h1 {
+    @apply mt-2 text-3xl;
+  }
+  h2 {
+    @apply mt-2 text-2xl;
+  }
+  h3 {
+    @apply mt-2 text-xl;
+  }
+  a {
+    @apply text-blue-600 hover:text-blue-800 hover:underline;
+  }
+  code {
+    @apply bg-gray-800 text-gray-400 px-1 rounded py-px;
+  }
+  blockquote {
+    @apply py-1 px-4  bg-gray-100 mb-4 border-l-4 border-gray-400;
+    & > blockquote {
+      @apply bg-gray-200;
+    }
+  }
+  pre {
+    @apply bg-gray-800 rounded-md;
+  }
+  ol {
+    @apply list-decimal ml-8;
+  }
+  ul {
+    @apply list-disc ml-8;
+  }
+  hr {
+    @apply my-2;
+  }
 }
 </style>
 
 <script>
 export default {
   async asyncData ({ $content, params }) {
+
     return {
       article: await $content('articles', params.slug).fetch()
     }

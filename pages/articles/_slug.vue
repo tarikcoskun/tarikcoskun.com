3074f180ce6ea0bdf8cd3b8788dc562edd46367c
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-05 12:06:09 +0200

    Update _slug.vue

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,8 +1,6 @@
 <template>
 <div class="p-4 md:px-48 md:py-8">
 
-  <NuxtLink to="/" class="inline-block p-3 ml-5 -my-2 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTYuNjcgMGwyLjgzIDIuODI5LTkuMzM5IDkuMTc1IDkuMzM5IDkuMTY3LTIuODMgMi44MjktMTIuMTctMTEuOTk2eiIvPjwvc3ZnPg=="></NuxtLink>
-
     <div class="grid gap-2 p-6">
     <div class="px-4 py-2 bg-gray-200 rounded-lg">
       <h1 class="text-2xl font-semibold text-gray-800">{{ article.title }}</h1>

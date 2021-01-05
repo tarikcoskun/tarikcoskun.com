dab12c5b595c1d46baace3a2e7ba984729cb9cfc
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-05 11:23:06 +0200

    Updated CSS

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -14,51 +14,51 @@
 </div>
 </template>
 
-<style lang="scss">
+<style lang="postcss">
 .nuxt-content {
   @apply leading-relaxed;
-  h1 {
-    @apply font-semibold mt-2 text-3xl;
-  }
-  h2 {
-    @apply font-semibold mt-2 text-2xl;
-  }
-  h3 {
-    @apply font-semibold mt-2 text-xl;
-  }
-  a {
-    @apply text-blue-600 hover:text-blue-800 hover:underline;
-  }
-  code {
-    @apply bg-gray-800 text-gray-400 px-1 rounded py-px;
-  }
-  blockquote {
-    @apply py-1 px-4  bg-gray-100 mb-4 border-l-4 border-gray-400;
-    & > blockquote {
-      @apply bg-gray-200;
-    }
-  }
-  pre {
-    @apply bg-gray-800 rounded-md;
-  }
-  ol {
-    @apply list-decimal ml-8;
-  }
-  ul {
-    @apply list-disc ml-8;
-  }
-  hr {
-    @apply my-2;
-  }
-  td, th {
-    @apply px-2 py-1 border border-gray-400;
-  }
-  tr:nth-child(odd) {
-    @apply bg-gray-100;
-  }
-  table {
-    @apply mb-6;
-  }
+}
+.nuxt-content h1 {
+  @apply font-semibold mt-2 text-3xl;
+}
+.nuxt-content h2 {
+  @apply font-semibold mt-2 text-2xl;
+}
+.nuxt-content h3 {
+  @apply font-semibold mt-2 text-xl;
+}
+.nuxt-content a {
+  @apply text-blue-600 hover:text-blue-800 hover:underline;
+}
+.nuxt-content code {
+  @apply bg-gray-800 text-gray-400 px-1 rounded py-px;
+}
+.nuxt-content blockquote {
+  @apply py-1 px-4  bg-gray-100 mb-4 border-l-4 border-gray-400;
+}
+.nuxt-content blockquote > blockquote {
+   @apply bg-gray-200;
+ }
+.nuxt-content pre {
+  @apply bg-gray-800 rounded-md;
+}
+.nuxt-content ol {
+  @apply list-decimal ml-8;
+}
+.nuxt-content ul {
+  @apply list-disc ml-8;
+}
+.nuxt-content hr {
+  @apply my-2;
+}
+.nuxt-content td, th {
+  @apply px-2 py-1 border border-gray-400;
+}
+.nuxt-content tr:nth-child(odd) {
+  @apply bg-gray-100;
+}
+.nuxt-content table {
+  @apply mb-6;
 }
 </style>
 

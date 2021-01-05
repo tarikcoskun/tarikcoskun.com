29368bfb3ca9fd43b04024ff838899e5adc9848b
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-05 11:03:58 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="p-4 md:px-16 md:py-8">
 
-  <div class="gap-4 md:grid md:grid-cols-7">
-    <div class="col-span-7 p-3 bg-gray-100 rounded-lg md:p-6 md:mb-0">
+  <div class="grid grid-cols-1 gap-4 md:grid-cols-7">
+    <div class="col-span-7 p-3 bg-gray-100 rounded-lg md:p-6">
       <h1 class="text-2xl font-semibold">About Me</h1>
       <p class="text-lg text-justify text-gray-700">Howdy! I'm Tarık, a young developer from Turkey. I spend most of my time playing games, coding and chatting. I have been coding since 2018, when I started using Discord. I've always wondered how Discord bots and websites work, so I started researching about them. About 8 months later, I had enough knowledge to create Discord bots from scratch. After learning about Discord bots, I started researching about websites and since then, I'm trying to create websites using new technologies I learn.</p>
     </div>
-    <section class="col-span-4 mt-4 md:mt-0">
-      <div class="grid gap-4">
-        <div class="p-3 mb-4 bg-gray-100 rounded-lg md:mb-0">
+    <section class="col-span-7 md:col-span-4">
+      <div class="grid grid-cols-1 gap-4">
+        <div class="p-3 bg-gray-100 rounded-lg">
           <h1 class="text-2xl font-semibold">Positions</h1>
-          <div class="grid gap-2 md:grid-cols-2">
+          <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
             <div>
               <a href="https://zyapimstudios.com/generations" target="_blank" class="text-xl font-semibold text-gray-800 hover:underline">Generations</a>
               <p class="-mt-1 text-lg text-gray-700">Website Developer</p>
@@ -38,11 +38,11 @@
         </div>
       </div>
     </section>
-    <section class="col-span-3">
+    <section class="col-span-7 md:col-span-3">
       <div class="grid gap-4">
         <div class="p-3 bg-gray-100 rounded-lg">
           <h1 class="text-2xl font-semibold">Skills</h1>
-          <div class="grid gap-2 md:grid-cols-2">
+          <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
             <div class="p-2 bg-gray-200 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl font-semibold text-gray-700 truncate">HTML</h1>
@@ -109,6 +109,24 @@
       </div>
     </section>
   </div>
+  <h1 class="m-4 text-3xl font-semibold text-center md:mt-4 md:m-0 md:text-2xl md:text-left">Latest Posts</h1>
+  <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
+    <nuxt-link :to="`/articles/${article.slug}`" class="p-3 bg-gray-700 rounded-lg hover:bg-gray-800" v-for="article in articles" :key="article.id">
+      <h1 class="text-2xl text-gray-200">{{ article.title }}</h1>
+      <p class="-mt-1 text-gray-400 truncate">{{ article.description }}</p>
+    </nuxt-link>
+  </div>
 
 </div>
 </template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+
+    return {
+      articles: await $content('articles', params.slug).fetch()
+    }
+  }
+}
+</script>

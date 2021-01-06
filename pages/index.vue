ea35e7ea66819d6920e36130bccf22c80b4ed1ca
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-06 18:40:16 +0200

    Updated layout

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@
             </div>
           </div>
         </div>
-        <div class="p-3 bg-purple-100 rounded-lg ">
+        <div class="hidden p-3 rounded-lg bg-purple-50 md:block ">
           <h1 class="inline text-2xl text-gray-700">Follow Me
             <a href="https://discord.com/users/474537652943847444" target="_blank"><svg class="inline fill-current hover:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z"/></svg></a>
             <a href="https://steamcommunity.com/id/tarikcoskun" target="_blank"><svg class="inline fill-current hover:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M24 12c0 6.627-5.373 12-12 12-5.782 0-10.608-4.091-11.744-9.537l4.764 2.003c.285 1.441 1.56 2.547 3.115 2.534 1.723-.017 3.105-1.414 3.116-3.129l.007-.003 3.602-2.684c2.304.024 4.14-1.833 4.14-4.091 0-2.26-1.834-4.093-4.093-4.093-2.234 0-4.048 1.791-4.09 4.015l-2.64 3.693c-.668-.014-1.269.169-1.791.51l-6.294-2.646c.708-5.953 5.765-10.572 11.908-10.572 6.627 0 12 5.373 12 12zm-16.577 5.477l-1.544-.649c.38.858 1.236 1.461 2.249 1.457 1.346-.012 2.422-1.11 2.41-2.455-.012-1.329-1.104-2.41-2.432-2.41-.287.001-.57.05-.86.16l1.542.648c.898.378 1.319 1.411.941 2.308-.376.896-1.409 1.318-2.306.941zm7.484-5.602c-1.533 0-2.781-1.249-2.781-2.782 0-1.534 1.248-2.782 2.781-2.782 1.534 0 2.781 1.248 2.781 2.782 0 1.533-1.247 2.782-2.781 2.782zm0-.682c1.159 0 2.1-.942 2.1-2.101 0-1.158-.94-2.102-2.1-2.102s-2.102.943-2.102 2.102c.001 1.159.943 2.101 2.102 2.101z"/></svg></a>
@@ -40,66 +40,66 @@
     </section>
     <section class="col-span-7 md:col-span-3">
       <div class="grid gap-4">
-        <div class="p-3 bg-purple-100 rounded-lg">
+        <div class="p-3 rounded-lg bg-purple-50">
           <h1 class="text-2xl text-gray-700">Skills</h1>
           <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
-            <div class="p-2 bg-purple-200 rounded-md bg-opacity-40">
+            <div class="p-2 bg-purple-100 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl text-gray-700 truncate">HTML</h1>
                 <span class="text-lg text-right text-gray-700">95%</span>
               </div>
-              <div class="flex h-2 overflow-hidden bg-purple-100 rounded">
+              <div class="flex h-2 overflow-hidden rounded bg-purple-50">
                 <div class="w-11/12 bg-gray-700"></div>
               </div>
             </div>
-            <div class="p-2 bg-purple-200 rounded-md bg-opacity-40">
+            <div class="p-2 bg-purple-100 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl text-gray-700 truncate">JavaScript</h1>
                 <span class="text-lg text-right text-gray-700">75%</span>
               </div>
-              <div class="flex h-2 overflow-hidden bg-purple-100 rounded">
+              <div class="flex h-2 overflow-hidden rounded bg-purple-50">
                 <div class="w-9/12 bg-gray-700"></div>
               </div>
             </div>
-            <div class="p-2 bg-purple-200 rounded-md bg-opacity-40">
+            <div class="p-2 bg-purple-100 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl text-gray-700 truncate">CSS</h1>
                 <span class="text-lg text-right text-gray-700">70%</span>
               </div>
-              <div class="flex h-2 overflow-hidden bg-purple-100 rounded">
+              <div class="flex h-2 overflow-hidden rounded bg-purple-50">
                 <div class="w-2/3 bg-gray-700"></div>
               </div>
             </div>
-            <div class="p-2 bg-purple-200 rounded-md bg-opacity-40">
+            <div class="p-2 bg-purple-100 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl text-gray-700 truncate">NodeJS</h1>
                 <span class="text-lg text-right text-gray-700">60%</span>
               </div>
-              <div class="flex h-2 overflow-hidden bg-purple-100 rounded">
+              <div class="flex h-2 overflow-hidden rounded bg-purple-50">
                 <div class="w-3/5 bg-gray-700"></div>
               </div>
             </div>
-            <div class="p-2 bg-purple-200 rounded-md bg-opacity-40">
+            <div class="p-2 bg-purple-100 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl text-gray-700 truncate">NuxtJS</h1>
                 <span class="text-lg text-right text-gray-700">15%</span>
               </div>
-              <div class="flex h-2 overflow-hidden bg-purple-100 rounded">
+              <div class="flex h-2 overflow-hidden rounded bg-purple-50">
                 <div class="w-1/6 bg-gray-700"></div>>
               </div>
             </div>
-            <div class="p-2 bg-purple-200 rounded-md bg-opacity-40">
+            <div class="p-2 bg-purple-100 rounded-md">
               <div class="flex items-center justify-between mb-1">
                 <h1 class="text-xl text-gray-700 truncate">TailwindCSS</h1>
                 <span class="text-lg text-right text-gray-700">40%</span>
               </div>
-              <div class="flex h-2 overflow-hidden bg-purple-100 rounded">
+              <div class="flex h-2 overflow-hidden rounded bg-purple-50">
                 <div class="w-2/5 bg-gray-700"></div>
               </div>
             </div>
           </div>
         </div>
-        <div class="p-3 bg-purple-100 rounded-lg md:hidden">
+        <div class="p-3 rounded-lg bg-purple-50 md:hidden">
           <h1 class="inline text-2xl text-gray-700">Follow Me
             <a href="https://discord.com/users/474537652943847444" target="_blank"><svg class="inline fill-current hover:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z"/></svg></a>
             <a href="https://steamcommunity.com/id/tarikcoskun" target="_blank"><svg class="inline fill-current hover:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M24 12c0 6.627-5.373 12-12 12-5.782 0-10.608-4.091-11.744-9.537l4.764 2.003c.285 1.441 1.56 2.547 3.115 2.534 1.723-.017 3.105-1.414 3.116-3.129l.007-.003 3.602-2.684c2.304.024 4.14-1.833 4.14-4.091 0-2.26-1.834-4.093-4.093-4.093-2.234 0-4.048 1.791-4.09 4.015l-2.64 3.693c-.668-.014-1.269.169-1.791.51l-6.294-2.646c.708-5.953 5.765-10.572 11.908-10.572 6.627 0 12 5.373 12 12zm-16.577 5.477l-1.544-.649c.38.858 1.236 1.461 2.249 1.457 1.346-.012 2.422-1.11 2.41-2.455-.012-1.329-1.104-2.41-2.432-2.41-.287.001-.57.05-.86.16l1.542.648c.898.378 1.319 1.411.941 2.308-.376.896-1.409 1.318-2.306.941zm7.484-5.602c-1.533 0-2.781-1.249-2.781-2.782 0-1.534 1.248-2.782 2.781-2.782 1.534 0 2.781 1.248 2.781 2.782 0 1.533-1.247 2.782-2.781 2.782zm0-.682c1.159 0 2.1-.942 2.1-2.101 0-1.158-.94-2.102-2.1-2.102s-2.102.943-2.102 2.102c.001 1.159.943 2.101 2.102 2.101z"/></svg></a>
@@ -120,7 +120,7 @@
 </div>
 </template>
 
-<!--<script>
+<script>
 export default {
   async asyncData ({ $content, params }) {
 
@@ -129,4 +129,4 @@ export default {
     }
   }
 }
-</script>-->
+</script>

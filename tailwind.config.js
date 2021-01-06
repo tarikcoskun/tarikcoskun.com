5bc87b807c685fda8626048d2dafa69fe4051afb
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-06 18:40:45 +0200

    Extended purple color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
+
 module.exports = {
   future: {},
   purge: [],
   theme: {
     extend: {
-      opacity: {
-        '40': '0.40',
+      colors: {
+        purple: {
+          50: '#FAF5FF',
+          100: '#F3E8FF',
+          200: '#E9D5FF',
+          300: '#D8B4FE',
+          400: '#C084FC',
+          500: '#A855F7',
+          600: '#9333EA',
+          700: '#7E22CE',
+          800: '#6B21A8',
+          900: '#581C87',
+        }
       }
     },
   },
   variants: {},
   plugins: [],
   experimental: {
-    applyComplexClasses: true
+    applyComplexClasses: true,
   }
 }

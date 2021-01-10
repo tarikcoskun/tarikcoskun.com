b3882f0353005fa040ae27fe8a77445719e646ef
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-10 14:28:29 +0200

    Updated title

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="mt-2 col-span-full">
-    <div class="p-3 bg-gray-200 border-2 border-gray-300 rounded-lg md:p-6">
-      <h1 class="text-3xl font-bold text-center">Coming Soon!</h1>
+  <section class="p-4 mb-2 md:mt-2 col-span-full md:py-12 md:px-24">
+    <div class="p-3 rounded-lg md:p-4">
+      <h1 class="text-6xl font-bold text-center">Coming Soon!</h1>
     </div>
   </section>
 </template>

0f09f2249fb0deeb95705211f7157e875f18efec
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-10 14:34:51 +0200

    Decreased vertical padding

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,21 +1,19 @@
 <template>
-  <section class="p-4 mb-2 md:mt-2 col-span-full md:py-12 md:px-24">
-    <div>
-      <div class="grid gap-2 md:gap-4 md:grid-cols-2">
-        <NuxtLink
-          to="#"
-          class="p-3 bg-teal-500 border-2 border-teal-600 rounded-lg"
-          v-for="item in projects"
-          :key="item"
-        >
-          <div class="py-16 mb-2 bg-teal-600 rounded-md"></div>
-          <h1 class="text-xl text-white">{{ item.title }}</h1>
-          <p class="text-justify text-gray-100 line-clamp-2">
-            {{ item.description }}
-          </p>
-        </NuxtLink>
-      </div>
-    </div>
+  <section
+    class="grid gap-2 p-4 mb-2 col-span-full md:gap-4 md:grid-cols-2 md:mt-2 md:pb-12 md:pt-6 md:px-24"
+  >
+    <NuxtLink
+      to="#"
+      class="p-3 bg-teal-500 border-2 border-teal-600 rounded-lg"
+      v-for="item in projects"
+      :key="item"
+    >
+      <div class="py-16 mb-2 bg-teal-600 rounded-md"></div>
+      <h1 class="text-xl text-white">{{ item.title }}</h1>
+      <p class="text-justify text-gray-100 line-clamp-2">
+        {{ item.description }}
+      </p>
+    </NuxtLink>
   </section>
 </template>
 

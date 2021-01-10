b2bfd81a687fd65f47ee8c6c7ed5226f2c2d2d30
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-10 11:25:08 +0200

    Updated layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="p-4 md:py-12 md:px-24">
+    <div class="grid grid-cols-4 gap-2 md:gap-4">
+      <!-- Navbar -->
+      <NuxtLink
+        to="/"
+        class="p-2 text-2xl text-center text-white bg-gray-800 border-2 border-gray-900 rounded-lg col-span-full md:col-span-1 hover:bg-gray-900"
+        >Home</NuxtLink
+      >
+      <NuxtLink
+        to="/projects"
+        class="p-2 text-2xl text-center text-white bg-teal-500 border-2 border-teal-600 rounded-lg col-span-full md:col-span-1 hover:bg-teal-600"
+        >Projects</NuxtLink
+      >
+      <NuxtLink
+        to="/blog"
+        class="p-2 text-2xl text-center text-white bg-red-500 border-2 border-red-600 rounded-lg col-span-full md:col-span-1 hover:bg-red-600"
+        >Blog</NuxtLink
+      >
+      <NuxtLink
+        to="/contact"
+        class="p-2 text-2xl font-bold text-center bg-gray-200 border-2 border-gray-300 rounded-lg col-span-full md:col-span-1 hover:bg-gray-300"
+        >Contact</NuxtLink
+      >
+      <!-- End of Navbar -->
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -18,8 +42,8 @@ html {
 }
 
 *,
-*:before,
-*:after {
+*::before,
+*::after {
   box-sizing: border-box;
   margin: 0;
 }

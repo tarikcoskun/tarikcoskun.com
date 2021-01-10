bab7c046ab8844bbf43753a1a0eaad154f6a2650
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-10 11:26:14 +0200

    Created blog page

---
+++ pages/blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="mt-2 col-span-full">
+    <div class="p-3 bg-gray-200 border-2 border-gray-300 rounded-lg md:p-6">
+      <h1 class="text-3xl font-bold text-center">Coming Soon!</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Blog - Tarık Coşkun',
+    }
+  },
+}
+</script>

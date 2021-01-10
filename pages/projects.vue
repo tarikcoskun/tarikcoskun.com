6c7cfe7fbbc4a349f1506048aa1b8731806e8854
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-10 13:06:41 +0200

    Updated color scheme

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,19 +1,22 @@
 <template>
-  <section class="mt-2 col-span-full">
-    <div class="p-3 bg-teal-500 border-2 border-teal-600 rounded-lg md:p-6">
-      <h1 class="mb-2 text-3xl text-center text-white">Projects</h1>
+  <section class="mb-2 md:mt-2 col-span-full">
+    <div
+      class="p-3 bg-gray-200 border-2 border-gray-300 border-opacity-50 rounded-lg md:p-4"
+    >
+      <h1 class="mb-2 text-3xl font-bold text-center">Projects</h1>
       <div class="grid gap-2 md:gap-4 md:grid-cols-2">
-        <div
-          class="p-3 bg-teal-600 rounded-lg"
+        <NuxtLink
+          to="#"
+          class="p-3 bg-teal-500 border-2 border-teal-600 rounded-lg"
           v-for="item in projects"
           :key="item"
         >
-          <div class="py-16 mb-2 bg-white rounded-md"></div>
+          <div class="py-16 mb-2 bg-teal-600 rounded-md"></div>
           <h1 class="text-xl text-white">{{ item.title }}</h1>
           <p class="text-justify text-gray-100 line-clamp-2">
             {{ item.description }}
           </p>
-        </div>
+        </NuxtLink>
       </div>
     </div>
   </section>
@@ -33,7 +36,13 @@ export default {
           image: '',
           title: 'Project Name',
           description:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, maiores sint obcaecati vel perspiciatis amet voluptas, consequatur nobis sequi qui enim facere non expedita, neque nemo tenetur magnam repellat officia dolore harum doloribus quod excepturi in hic? Quibusdam, neque sapiente voluptatibus pariatur sint numquam impedit iure laboriosam recusandae, vitae qui?',
+        },
+        {
+          image: '',
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, maiores sint obcaecati vel perspiciatis amet voluptas, consequatur nobis sequi qui enim facere non expedita, neque nemo tenetur magnam repellat officia dolore harum doloribus quod excepturi in hic? Quibusdam, neque sapiente voluptatibus pariatur sint numquam impedit iure laboriosam recusandae, vitae qui?',
         },
       ],
     }

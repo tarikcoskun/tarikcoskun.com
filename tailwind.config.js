39ded69974cf4c518f3dce9565c26263b8843572
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-10 11:33:20 +0200

    Extended width value, added line-clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,15 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      colors: {
-        purple: {
-          50: '#FAF5FF',
-          100: '#F3E8FF',
-          200: '#E9D5FF',
-          300: '#D8B4FE',
-          400: '#C084FC',
-          500: '#A855F7',
-          600: '#9333EA',
-          700: '#7E22CE',
-          800: '#6B21A8',
-          900: '#581C87',
-        }
+      width: {
+        '28': '7rem'
       }
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
   experimental: {
     applyComplexClasses: true,
   }

36c86540c319320353f36378270e48063fb0c2a7
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-11 23:03:24 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
         media: 'all',
       }
     ],

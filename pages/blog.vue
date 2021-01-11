547d5f07a83b13436a0547158c37c55677ddb188
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-11 16:43:17 +0200

    Update blog.vue

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,8 +1,18 @@
 <template>
-  <section class="p-4 mb-2 md:mt-2 col-span-full md:py-12 md:px-24">
-    <div class="p-3 rounded-lg md:p-4">
-      <h1 class="text-6xl font-bold text-center">Coming Soon!</h1>
-    </div>
+  <section
+    class="grid gap-2 p-4 mb-2 col-span-full md:gap-4 md:grid-cols-2 md:pb-12 md:px-40"
+  >
+    <nuxt-link
+      :to="`/articles/${article.slug}`"
+      class="p-3 bg-gray-800 border-2 border-gray-900 rounded-lg hover:bg-gray-900"
+      v-for="article in articles"
+      :key="article.id"
+    >
+      <h1 class="text-2xl text-white">{{ article.title }}</h1>
+      <p class="-mt-1 text-gray-100 line-clamp-2">
+        {{ article.description }}
+      </p>
+    </nuxt-link>
   </section>
 </template>
 
@@ -11,6 +21,11 @@ export default {
   head() {
     return {
       title: 'Blog - Tarık Coşkun',
+    }
+  },
+  async asyncData({ $content, params }) {
+    return {
+      articles: await $content('articles', params.slug).fetch(),
     }
   },
 }

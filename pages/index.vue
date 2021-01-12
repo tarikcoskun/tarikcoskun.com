2f2d03ca74f263858b4c16e8ea1cf1f147f559fa
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-12 10:37:30 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     class="grid gap-4 p-4 mb-2 md:grid-cols-2 col-span-full md:pb-12 md:px-40"
   >
     <div
-      class="px-3 py-2 bg-gray-800 border-2 border-gray-900 border-opacity-50 rounded-lg md:col-span-full md:p-4"
+      class="px-3 py-2 bg-gray-900 border-2 rounded-lg md:col-span-full md:p-4"
     >
       <h1 class="text-2xl text-center text-white">About Me</h1>
       <p class="text-lg text-justify text-gray-200">
@@ -16,7 +16,7 @@
         and Tailwind CSS and this website is only example for now.
       </p>
     </div>
-    <div class="px-3 py-2 bg-gray-100 border-2 border-gray-200 rounded-lg">
+    <div class="px-3 py-2 bg-gray-100 rounded-lg">
       <h2 class="my-2 text-2xl font-bold text-center">
         Technologies and tools I use
       </h2>
@@ -33,7 +33,7 @@
         </div>
       </div>
     </div>
-    <div class="px-3 py-2 bg-gray-100 border-2 border-gray-200 rounded-lg">
+    <div class="px-3 py-2 bg-gray-100 rounded-lg">
       <h2 class="my-2 text-2xl font-bold text-center">
         Technologies I want to learn
       </h2>

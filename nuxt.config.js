ac5fcd38ada7bfff19c791640db563e43162c44c
Tarık <44239968+tarikcoskun@users.noreply.github.com>
2021-01-12 10:31:12 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'theme-color', content: '#1a202c' },
+      { name: 'theme-color', content: '#111827' },
       {
         name: 'description',
         content: 'Howdy! I\'m Tarık, a young Front-End developer from Turkey. I spend most of my time playing games, coding and chatting. I have been coding since 2018, when I started using Discord. I\'ve always wondered how Discord bots work, so I started coding Discord bots. In 2019, I started creating Discord bot dashboards and since then, I\'m trying to create websites using new technologies I learn. In late 2020, I started learning Nuxt.js and Tailwind CSS and this website is only example for now.',

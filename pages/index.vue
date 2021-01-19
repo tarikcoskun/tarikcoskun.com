ea94ff02ec17633cfd5d7613b47ee52908f40123
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-01-19 14:58:45 +0200

    :art: Edit titles

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
     </div>
     <div class="px-3 py-2 bg-gray-50 rounded-lg">
       <h2 class="my-2 text-2xl font-bold text-center">
-        Technologies and tools I use
+        Technologies & Tools
       </h2>
       <div
         class="flex flex-wrap items-center justify-center mt-4 text-gray-600"
@@ -35,7 +35,7 @@
     </div>
     <div class="px-3 py-2 bg-gray-50 rounded-lg">
       <h2 class="my-2 text-2xl font-bold text-center">
-        Technologies I want to learn
+        Technologies Learning
       </h2>
       <div
         class="flex flex-wrap items-center justify-center mt-4 text-gray-600"

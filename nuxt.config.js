81eca94c7e38116f1519a0bd8a3ce9b0eda702ae
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-01-19 11:26:56 +0200

    :wrench: Edit meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+let description = 'Howdy! I\'m Tarık, a young Front-End developer from Turkey. I spend most of my time playing games, coding and chatting. I have been coding since 2018, when I started using Discord. I\'ve always wondered how Discord bots work, so I started coding Discord bots. In 2019, I started creating Discord bot dashboards and since then, I\'m trying to create websites using new technologies I learn. In late 2020, I started learning Nuxt.js and Tailwind CSS and this website is only example for now.'
+
 export default {
   ssr: false,	
   target: 'static',
@@ -8,8 +10,19 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'theme-color', content: '#111827' },
       {
+        name: 'og:site_name',
+        hid: 'og:site_name',
+        content: 'tarikcoskun.xyz',
+      },
+      {
         name: 'description',
-        content: 'Howdy! I\'m Tarık, a young Front-End developer from Turkey. I spend most of my time playing games, coding and chatting. I have been coding since 2018, when I started using Discord. I\'ve always wondered how Discord bots work, so I started coding Discord bots. In 2019, I started creating Discord bot dashboards and since then, I\'m trying to create websites using new technologies I learn. In late 2020, I started learning Nuxt.js and Tailwind CSS and this website is only example for now.',
+        hid: 'description',
+        content: description,
+      },
+      {
+        name: 'og:description',
+        hid: 'og:description',
+        content: description,
       },
     ],
     link: [

e726323e3fed0e6d6a259e5649b363d4cd8d676a
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-01-30 14:20:29 +0200

    :wrench: Apply main styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,34 @@
   </div>
 </template>
 
-<style>
+<style lang="postcss">
 html {
   font-family: 'Roboto', sans-serif;
+}
+body {
+  @apply bg-gray-50 transform duration-75;
+}
+section {
+  @apply grid gap-4 md:gap-y-12;
+}
+h1 {
+  @apply mb-2 text-3xl font-bold text-center text-gray-800 md:text-left;
+}
+h2 {
+  @apply text-2xl font-bold text-center text-gray-800;
+}
+p {
+  @apply text-lg text-justify text-gray-700;
+}
+.dark-mode body {
+  @apply bg-gray-800;
+}
+.dark-mode h1,
+.dark-mode h2 {
+  @apply text-gray-100 font-normal;
+}
+.dark-mode p {
+  @apply text-gray-300;
 }
 </style>
 

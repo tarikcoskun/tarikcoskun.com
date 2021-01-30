bf3ae3e3df57fd04863bee46fe13967e02951245
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-01-30 15:11:22 +0200

    :sparkles: Add components

---
+++ components/Introduction.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Howdy, visitor!</h1>
+    <p>
+      I'm Tarık, a young Front-End developer from Turkey. I have been coding
+      since 2018, when I started using Discord. I've always wondered how Discord
+      bots work, so I started coding Discord bots. In 2019, I started creating
+      Discord bot dashboards and since then, I'm creating websites using new
+      technologies I learn.
+    </p>
+  </div>
+</template>

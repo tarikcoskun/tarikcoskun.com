b336c232c75295de602bc39a8c90aaea3fc712bb
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-02-03 17:42:43 +0300

    :wrench: Update text

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -2,11 +2,10 @@
   <div>
     <h1>Howdy, visitor!</h1>
     <p>
-      I'm Tarık, a young Front-End developer from Turkey. I have been coding
+      I'm Tarık, a young Front-End web developer from Turkey. I have been coding
       since 2018, when I started using Discord. I've always wondered how Discord
-      bots work, so I started coding Discord bots. In 2019, I started creating
-      Discord bot dashboards and since then, I'm creating websites using new
-      technologies I learn.
+      bots work, so I started coding Discord bots. Since 2019, I'm creating
+      websites using new technologies I learn.
     </p>
   </div>
 </template>

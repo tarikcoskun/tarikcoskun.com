048da296a55ac67091fe6ab06981c3af77ea86c0
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-02-13 17:05:51 +0300

    :wrench: Minor changes

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
-    <h1>Projects</h1>
-    <div class="grid">
+    <h2>Projects</h2>
+    <div class="grid gap-4 lg:grid-cols-2">
       <a
         :href="project.link"
         target="_blank"
         v-for="project in projects"
         :key="project"
-        class="project"
+        class="flex flex-shrink"
       >
-        <img :src="project.photo" draggable="false" class="w-auto h-auto md:w-80 md:rounded-l-lg md:rounded-t-none rounded-t-md" />
-        <div class="p-2 space-y-2 md:p-6">
-          <h1 :title="project.title" class="project-title">{{ project.title }}</h1>
-          <p class="hidden text-lg leading-6 md:block">{{ project.description }}</p>
+        <img
+          :src="project.photo"
+          draggable="false"
+          class="rounded-lg"
+          style="width: 100px; height: 100px"
+        />
+        <div class="pl-3">
+          <h1 :title="project.title" class="project-title">
+            {{ project.title }}
+          </h1>
+          <p class="text-base leading-6 text-left line-clamp-3">
+            {{ project.description }}
+          </p>
         </div>
       </a>
     </div>
@@ -20,18 +29,9 @@
 </template>
 
 <style lang="postcss">
-.project {
-  @apply md:flex bg-gray-200 rounded-md md:rounded-lg hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400;
-}
 .project-title {
-  @apply text-xl md:text-2xl text-center md:text-left text-gray-800 truncate;
+  @apply text-lg text-left truncate;
   margin-bottom: -0.05rem;
-}
-.dark-mode .project {
-  @apply bg-gray-700 hover:bg-gray-600 focus:ring-gray-500;
-}
-.dark-mode .project-title {
-  @apply text-gray-200 font-normal;
 }
 </style>
 
@@ -41,11 +41,22 @@ export default {
     return {
       projects: [
         {
-          title: 'Flowey\'s Time Machine Rework',
+          title: "Flowey's Time Machine Rework",
           link: 'http://crumblingstatue.github.io/FloweysTimeMachine',
           badge: 'Designer',
-          photo: 'https://cdn.discordapp.com/attachments/801123953187749918/804766073697075240/Screen_Shot_2021-01-29_at_23.02.56.png',
-          description :'Flowey\'s Time Machine is an UNDERTALE save editor that allows you to change everything about your save easily',
+          photo:
+            'https://media.discordapp.net/attachments/705535550093787296/810121912276746241/Adsz.png',
+          description:
+            "Flowey's Time Machine is an UNDERTALE save editor that allows you to change everything about your save easily",
+        },
+        {
+          title: "Flowey's Time Machine Rework",
+          link: 'http://crumblingstatue.github.io/FloweysTimeMachine',
+          badge: 'Designer',
+          photo:
+            'https://media.discordapp.net/attachments/705535550093787296/810121912276746241/Adsz.png',
+          description:
+            "Flowey's Time Machine is an UNDERTALE save editor that allows you to change everything about your save easily",
         },
       ],
     }

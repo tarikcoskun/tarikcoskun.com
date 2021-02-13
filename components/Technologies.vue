5ea2b81df10ad322707ea4aa99ac8158c5ea7a62
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-02-14 00:44:25 +0300

    :art: Change some colors

--- components/Technologies.vue
+++ components/Technologies.vue
@@ -31,7 +31,7 @@ export default {
           title: 'HTML',
           link: 'https://developer.mozilla.org/tr/docs/Web/HTML',
           background:
-            'background-image: linear-gradient(to bottom right, #ec8052, #f4470b)',
+            'background-image: linear-gradient(to bottom right, #f0936c, #e03a00)',
           icon:
             '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z" /></svg>',
         },
@@ -39,7 +39,7 @@ export default {
           title: 'CSS',
           link: 'https://developer.mozilla.org/tr/docs/Web/CSS',
           background:
-            'background-image: linear-gradient(to bottom right, #36a6d7, #2862e9)',
+            'background-image: linear-gradient(to bottom right, #5da2f9, #3659d6)',
           icon:
             '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z" /></svg>',
         },
@@ -63,7 +63,7 @@ export default {
           title: 'Express.js',
           link: 'http://expressjs.com',
           background:
-            'background-image: linear-gradient(to bottom right, #5da2f9, #3659d6)',
+            'background-image: linear-gradient(to bottom right, #66baff, #1490f5)',
           icon:
             '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"class="-ml-1" width="3rem"><path d="M32 24.795c-1.164.296-1.884.013-2.53-.957l-4.594-6.356-.664-.88-5.365 7.257c-.613.873-1.256 1.253-2.4.944l6.87-9.222-6.396-8.33c1.1-.214 1.86-.105 2.535.88l4.765 6.435 4.8-6.4c.615-.873 1.276-1.205 2.38-.883l-2.48 3.288-3.36 4.375c-.4.5-.345.842.023 1.325L32 24.795zM.008 15.427l.562-2.764C2.1 7.193 8.37 4.92 12.694 8.3c2.527 1.988 3.155 4.8 3.03 7.95H1.48c-.214 5.67 3.867 9.092 9.07 7.346 1.825-.613 2.9-2.042 3.438-3.83.273-.896.725-1.036 1.567-.78-.43 2.236-1.4 4.104-3.45 5.273-3.063 1.75-7.435 1.184-9.735-1.248C1 21.6.434 19.812.18 17.9c-.04-.316-.12-.617-.18-.92q.008-.776.008-1.552zm1.498-.38h12.872c-.084-4.1-2.637-7.012-6.126-7.037-3.83-.03-6.58 2.813-6.746 7.037z"/></svg>',
         },

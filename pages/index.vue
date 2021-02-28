ad5171977eb2fbc4171453512db5c71c9d514649
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-02-28 13:52:19 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <section>
-    <Introduction />
+   <div>
+      <h1>Howdy, visitor!</h1>
+      <p>
+        I'm Tarık, a young Front-End web developer from Turkey. I have been coding
+        since 2018, when I started using Discord. I've always wondered how Discord
+        bots work, so I started coding Discord bots. Since 2019, I'm creating
+        websites using new technologies I learn.
+      </p>
+    </div>
     <Technologies />
-    <Repos />
     <Projects />
     <Contact />
   </section>

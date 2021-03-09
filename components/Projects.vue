739fcb0cf37259e3ad9c8cb80ac6315bd755b056
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-03-09 19:52:07 +0300

    Update Projects.vue

--- components/Projects.vue
+++ components/Projects.vue
@@ -7,7 +7,7 @@
         :key="project"
         :href="project.link"
         target="_blank"
-        class="select-none flex background rounded-md transform duration-300 hover:-translate-y-1.5 border-gray-200 border shadow hover:shadow-lg"
+        class="select-none flex background rounded-md transform duration-300 hover:-translate-y-1.5 border-gray-200 border shadow-md hover:shadow-lg"
         v-wave
       >
         <div class="rounded-full" :style="project.color"></div>
@@ -32,12 +32,17 @@ export default {
         {
           color: "background: #d6212a; width: 8px; margin: 0.5rem;",
           title: "Flowey's Time Machine Rework",
-          link: 'https://floweystimemachine.github.io',
-          badge: 'Designer',
-          photo: 'https://tarikcoskun.xyz/flowey.png',
+          link: "https://floweystimemachine.github.io",
           description:
             "Flowey's Time Machine is an UNDERTALE save editor that allows you to change everything about your save easily",
         },
+        {
+          color: "background: #7289da; width: 8px; margin: 0.5rem;",
+          title: "My PreMiD Presences",
+          link: "https://premid.app/users/474537652943847444",
+          description:
+            "PreMiD is a simple, configurable utility that allows you to show what you're doing on the web in your Discord now playing status."
+        }
       ],
     }
   },

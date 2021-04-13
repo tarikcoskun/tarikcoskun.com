5b3042ad4f5e8f0aa53560690af11bb8078b9064
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-04-13 17:40:34 +0300

    minor changes

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="absolute top-0 left-0 w-full h-2 bg-blue-500"></div>
+    <div
+      class="absolute top-0 left-0 w-full h-2"
+      style="background: #374151"
+    ></div>
     <Nuxt />
   </div>
 </template>

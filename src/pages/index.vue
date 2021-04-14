ebf2407604fc6ef857cefcc31c06c487fe1771e4
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-04-14 17:31:51 +0300

    style: mobile padding

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap items-center md:flex-nowrap gap-x-16">
+  <div class="flex flex-wrap items-center md:flex-nowrap gap-x-16 gap-y-8">
     <section class="w-96">
       <img
         :src="require('~/assets/icons/avatar.png')"

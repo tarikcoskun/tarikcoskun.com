24feaabe686343ed70810f351cd8329b456e4ad1
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-07-02 22:00:08 +0300

    new favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,8 +24,7 @@ export default {
         targets: ".anime-accounts",
         translateY: [15, 0],
         opacity: 1,
-        duration: 1000,
-        delay: anime.stagger(250, { start: 1000 }),
+        delay: anime.stagger(200, { start: 1000 }),
         easing: "spring(1, 80, 15, 0)",
       });
 
@@ -33,7 +32,7 @@ export default {
         targets: ".anime-cards",
         translateY: [25, 0],
         opacity: 1,
-        delay: anime.stagger(250, { start: 1000 }),
+        delay: anime.stagger(200, { start: 1000 }),
         easing: "spring(1, 80, 10, 0)",
       });
     },

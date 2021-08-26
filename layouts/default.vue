512ec8ba26e94d57f1a87523a530cdacce3e535a
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-08-26 17:41:29 +0300

    ⬇️ Pascal cased name

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@
         "
       >
         <h1 class="hidden text-4xl font-medium font-title md:block">
-          Tarık COŞKUN
+          Tarık Coşkun
         </h1>
         <div class="flex items-center gap-5 ml-4 md:ml-0">
           <NuxtLink to="/#home">Home</NuxtLink>

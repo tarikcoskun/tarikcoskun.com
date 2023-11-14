d1935dd4937b7ce3966d5f853e9c594675b3c512
Tarık Coşkun <tarikcskun@gmail.com>
2023-11-14 17:56:55 +0300

    Implement slider for works

--- data/works.ts
+++ data/works.ts
@@ -1,37 +1,40 @@
 interface Work {
   link: string;
-  image: string;
+  logo: string;
   title: string;
   description: string;
   startDate: string;
   endDate: string;
+  slides?: string[][];
 }
 
 export const works: Work[] = [
   {
     link: "https://vocoapp.com",
-    image: "/works/voco.png",
+    logo: "/works/voco/logo.png",
     title: "Voco Inc.",
-    description:
-      "Voco is a unique mobile platform where users can freely showcase their creativity in organizing events. Thanks to this platform, individuals and institutions can plan and participate in events that suit their interests.",
+    description: "A mobile platform where users can freely showcase their creativity in organizing events.",
     startDate: "01 Aug 2023",
     endDate: "01 Oct 2023",
   },
   {
     link: "https://eren.si/en",
-    image: "/works/erensibot.png",
-    title: "ErensiBOT",
-    description:
-      "ErensiBot is an easy-to-use advanced Discord bot that is trusted by half a million Discord servers to manage their community.",
+    logo: "/works/erensibot/logo.png",
+    title: "ErensiBot",
+    description: "A Discord bot that is trusted by half a million Discord servers to manage their community.",
     startDate: "01 Apr 2023",
     endDate: "01 Sep 2023",
+    slides: [
+      ["/works/erensibot/slide1.png", "ErensiBot dashboard view"],
+      ["/works/erensibot/slide2.png", "ErensiBot features"],
+      ["/works/erensibot/slide3.png", "ErensiBot reaction role"],
+    ],
   },
   {
     link: "https://playpad.app",
-    image: "/works/playpad.png",
+    logo: "/works/playpad/logo.png",
     title: "PlayPad",
-    description:
-      "The ultimate choice of crypto investors to invest in high quality blockchain projects. The fastest and the most cost effective way of investing in crypto.",
+    description: "The fastest and the most cost effective way of investing in crypto.",
     startDate: "01 Oct 2022",
     endDate: "01 Apr 2023",
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Layout from "@/ui/Layout";
 import Box from "@/ui/Box";
 import Section from "@/ui/Section";
 import Image from "next/image";
+import Slider from "@/ui/Slider";
 import { ArrowUpRightIcon } from "@/ui/Icon";
 
 // Data
@@ -44,7 +45,7 @@ export default function Home() {
       <Box className="overflow-hidden">
         <Section>
           <h2 className="font-serif text-xl font-medium text-theme-heading">Experiences</h2>
-          <div className="mt-6 flex flex-col gap-y-12">
+          <div className="mt-6 flex flex-col gap-y-16">
             {works.map((work, idx) => (
               <Work {...work} key={idx} />
             ))}
@@ -57,28 +58,34 @@ export default function Home() {
 
 function Work(work: (typeof works)[number]) {
   return (
-    <article className="flex items-start gap-x-4">
-      <a
-        href={work.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="group relative h-12 w-12 shrink-0 overflow-hidden rounded-lg ring-1 ring-theme-outline ring-offset-2 ring-offset-theme-background"
-      >
-        <Image src={work.image} alt={work.title} width={48} height={48} className="h-full w-full rounded-lg" />
-        <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:opacity-100">
-          <ArrowUpRightIcon width={24} height={24} className="text-white" />
+    <article>
+      <header className="flex items-center gap-x-4">
+        <a
+          href={work.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="group relative h-12 w-12 shrink-0 overflow-hidden rounded-lg ring-1 ring-theme-outline ring-offset-2 ring-offset-theme-background"
+        >
+          <Image src={work.logo} alt={work.title} width={48} height={48} className="h-full w-full rounded-lg" />
+          <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/50 opacity-0 transition-opacity group-hover:opacity-100">
+            <ArrowUpRightIcon width={24} height={24} className="text-white" />
+          </div>
+        </a>
+        <div>
+          <h3 className="leading-none">
+            <a href={work.link} target="_blank" rel="noopener noreferrer" className="link font-medium">
+              {work.title}
+            </a>
+          </h3>
+          <span className="text-sm leading-none text-theme-subtext">
+            {new Date(work.startDate).getFullYear()}-{new Date(work.endDate).getFullYear().toString().substring(2)}
+          </span>
         </div>
-      </a>
-      <div className="flex flex-col gap-y-2">
-        <h3 className="leading-none">
-          <a href={work.link} target="_blank" rel="noopener noreferrer" className="link font-medium">
-            {work.title}
-          </a>
-        </h3>
-        <span className="text-sm leading-none text-theme-subtext">
-          {new Date(work.startDate).getFullYear()}-{new Date(work.endDate).getFullYear().toString().substring(2)}
-        </span>
+      </header>
+
+      <div className="mt-4">
         <p>{work.description}</p>
+        {work.slides && <Slider slides={work.slides} />}
       </div>
     </article>
   );

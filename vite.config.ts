0fa41fc7c08ac82ba9d600a26472c259db33ae25
Tarık Coşkun <tarikcskun@gmail.com>
2025-05-13 18:38:26 +0300

    add the correct `base` to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { resolve } from "node:path"
 
 export default defineConfig({
+	base: "/tarikcoskun.com",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
